86463ecdd31f6aad3e3fec38b83edf8751f0fc96
Mykola Kantsir <panmukola@gmail.com>
2024-10-15 18:52:46 +0200

    Refactor login_view to use @csrf_exempt decorator

--- web/views.py
+++ web/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate, logout
 from django.http import JsonResponse, HttpResponse
 from django.middleware.csrf import get_token
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
