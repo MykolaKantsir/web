63946dfc6986665f7a278c212e969dc84f463984
Mykola Kantsir <panmukola@gmail.com>
2025-11-25 22:26:28 +0100

    fix: Dashboard airport view JS scope bug and improve status badge styling

--- web/api_views.py
+++ web/api_views.py
@@ -23,7 +23,7 @@ class CustomAuthToken(ObtainAuthToken):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data['user'] #type: ignore
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
